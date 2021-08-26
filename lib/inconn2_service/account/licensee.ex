49d721d478ffe10281809f2a109ca2e5e8ae3bc9
ramaa balaji <ramaa_t@yahoo.com>
2021-08-26 15:09:13 +0900

    Party table creation

--- lib/inconn2_service/account/licensee.ex
+++ lib/inconn2_service/account/licensee.ex
@@ -8,6 +8,12 @@ defmodule Inconn2Service.Account.Licensee do
   schema "licensees" do
     field :company_name, :string
     field :sub_domain, :string
+    # possible values for party_type [AO,SELF,SP]
+    # if party_type is [AO, SELF] then other_party_type is nil
+    # if party_type is [AO, SP] then other_party_type is SP
+    # if party_type is [SP, AO] then other_party_type is AO
+    field :party_type, {:array, :string}, default: ["AO", "SELF"]
+    field :other_party_type, :string
     embeds_one :address, AddressEmbed, on_replace: :delete
     embeds_one :contact, ContactEmbed, on_replace: :delete
     belongs_to :business_type, BusinessType
@@ -18,7 +24,7 @@ defmodule Inconn2Service.Account.Licensee do
   @doc false
   def changeset(licensee, attrs) do
     licensee
-    |> cast(attrs, [:company_name, :business_type_id, :sub_domain])
+    |> cast(attrs, [:company_name, :business_type_id, :sub_domain, :party_type, :other_party_type])
     |> validate_required([:company_name, :business_type_id, :sub_domain])
     |> unique_constraint(:company_name)
     |> unique_constraint(:sub_domain)
