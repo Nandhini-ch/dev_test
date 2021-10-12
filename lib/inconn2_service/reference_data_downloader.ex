20ce18c2fcfe17e25d0c4b8bb21e5a98130ee17e
Minat Silvester <31278967+minatsilvester@users.noreply.github.com>
2021-10-12 17:15:45 +0530

    Bulk download for equipment and site

--- lib/inconn2_service/reference_data_downloader.ex
+++ lib/inconn2_service/reference_data_downloader.ex
@@ -16,4 +16,32 @@ defmodule Inconn2Service.ReferenceDataDownloader do
     final_report
   end
 
+  def download_equipments(prefix) do
+    locations = AssetConfig.list_equipments(prefix)
+
+    header = [["id", "reference", "Name", "Equipment Code", "Site Id", "Location Id", "Asset Category Id", "Connections In", "Connections Out", "Parent Id", "parent reference"]]
+
+    body =
+      Enum.map(locations, fn r ->
+        [r.id, "", r.name, r.equipment_code, r.site_id, r.location_id, r.asset_category_id, r.connections_in, r.connections_out, List.last(r.path), ""]
+      end)
+
+    final_report = header ++ body
+    final_report
+  end
+
+  def download_sites(prefix) do
+    locations = AssetConfig.list_sites(prefix)
+
+    header = [["id", "reference", "Name", "Description", "Branch", "Latitude", "Longitude", "Fencing Radius", "Site Code", "Time Zone", "Party Id"]]
+
+    body =
+      Enum.map(locations, fn r ->
+        [r.id, "", r.name, r.description, r.branch, r.latitude, r.longitude, r.fencing_radius, r.site_code, r.time_zone, r.party_id]
+      end)
+
+    final_report = header ++ body
+    final_report
+  end
+
 end
