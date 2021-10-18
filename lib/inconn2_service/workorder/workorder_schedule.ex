b20a5f1486116970af588b3c9faabc1d705e9a99
Harisudhan V R <ravihari215@gmail.com>
2021-10-18 10:59:21 +0530

    Modified workorder_schedule.ex

--- lib/inconn2_service/workorder/workorder_schedule.ex
+++ lib/inconn2_service/workorder/workorder_schedule.ex
@@ -8,6 +8,7 @@ defmodule Inconn2Service.Workorder.WorkorderSchedule do
     belongs_to :workorder_template, WorkorderTemplate
     field :asset_id, :integer
     field :asset_type, :string
+    field :holidays, {:array, :integer}, default: []
     field :first_occurrence_date, :date
     field :first_occurrence_time, :time
     field :next_occurrence_date, :date
@@ -19,7 +20,7 @@ defmodule Inconn2Service.Workorder.WorkorderSchedule do
   @doc false
   def changeset(workorder_schedule, attrs) do
     workorder_schedule
-    |> cast(attrs, [:workorder_template_id, :asset_id, :first_occurrence_date, :first_occurrence_time])
+    |> cast(attrs, [:workorder_template_id, :asset_id, :holidays, :first_occurrence_date, :first_occurrence_time])
     |> validate_required([:workorder_template_id, :asset_id, :first_occurrence_date, :first_occurrence_time])
     |> assoc_constraint(:workorder_template)
   end
--- priv/repo/tenant_migrations/20210913053646_create_workorder_schedules.exs
+++ priv/repo/tenant_migrations/20210913053646_create_workorder_schedules.exs
@@ -6,6 +6,7 @@ defmodule Inconn2Service.Repo.Migrations.CreateWorkorderSchedules do
       add :workorder_template_id, references(:workorder_templates, on_delete: :nothing)
       add :asset_id, :integer
       add :asset_type, :string
+      add :holidays, {:array, :integer}, default: []
       add :first_occurrence_date, :date
       add :first_occurrence_time, :time
       add :next_occurrence_date, :date
