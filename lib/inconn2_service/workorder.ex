4ea09c9ffab03ad1bcd56c1e99caa8a794cd2e7b
Minat Silvester K <minatsilvester>
2022-01-24 15:11:47 +0530

    production first commit

--- lib/inconn2_service/workorder.ex
+++ lib/inconn2_service/workorder.ex
@@ -1219,7 +1219,7 @@ defmodule Inconn2Service.Workorder do
       workpermit_checks =
         if workorder_template.workpermit_required do
           query = from wc in WorkorderCheck, where: wc.work_order_id == ^wo.id and wc.type == ^"WP"
-          Repo.all(query, prefix)
+          Repo.all(query, prefix: prefix)
         else
           []
         end
@@ -1227,7 +1227,7 @@ defmodule Inconn2Service.Workorder do
       loto_checks =
         if workorder_template.loto_required do
           query = from wc in WorkorderCheck, where: wc.work_order_id == ^wo.id and wc.type == ^"LOTO"
-          Repo.all(query, prefix)
+          Repo.all(query, prefix: prefix)
         else
           []
         end
@@ -1235,7 +1235,7 @@ defmodule Inconn2Service.Workorder do
       pre_checks =
         if workorder_template.pre_check_required do
           query = from wc in WorkorderCheck, where: wc.work_order_id == ^wo.id and wc.type == ^"PRE"
-          Repo.all(query, prefix)
+          Repo.all(query, prefix: prefix)
         else
           []
         end
