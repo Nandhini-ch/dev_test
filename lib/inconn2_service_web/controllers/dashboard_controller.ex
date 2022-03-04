4fae8e47d0c8dd3090148a50e57173d39e45d343
Harisudhan V R <ravihari215@gmail.com>
2022-03-04 16:46:04 +0530

    Entering metering data in separate table

--- lib/inconn2_service_web/controllers/dashboard_controller.ex
+++ lib/inconn2_service_web/controllers/dashboard_controller.ex
@@ -8,6 +8,11 @@ defmodule Inconn2ServiceWeb.DashboardController do
     render(conn, "energy_meter.json", energy_meter_data: energy_meter_data)
   end
 
+  def get_energy_meter_speedometer(conn, _) do
+    energy_meter_data = Dashboards.get_energy_meter_speedometer_random(conn.query_params, conn.assigns.sub_domain_prefix)
+    render(conn, "energy_meter.json", energy_meter_data: energy_meter_data)
+  end
+
   def get_workflow_ticket_pie_chart(conn, _) do
     workflow_data = Dashboards.ticket_linear_chart(conn.assigns.sub_domain_prefix, conn.query_params)
     render(conn, "workflow_linear_data.json", workflow_data: workflow_data)
@@ -18,10 +23,6 @@ defmodule Inconn2ServiceWeb.DashboardController do
     render(conn, "workflow_linear_data.json", workflow_data: workflow_data)
   end
 
-  def get_energy_meter_speedometer(conn, _) do
-    energy_meter_data = Dashboards.get_energy_meter_speedometer_random(conn.query_params, conn.assigns.sub_domain_prefix)
-    render(conn, "energy_meter.json", energy_meter_data: energy_meter_data)
-  end
   # def get_work_order_pie_chart(conn, _) do
   #   work_order_counts = Dashboard.work_order_pie_chart(conn.assigns.sub_domain_prefix, conn.query_params)
   #   render(conn, "work_order_pie.json", work_order_counts: work_order_counts)
