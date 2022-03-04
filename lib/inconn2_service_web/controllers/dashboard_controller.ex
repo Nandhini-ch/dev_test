acc1e5b5b706f720ca192a13dcafa24609af052a
Minat Silvester K <minatsilvester>
2022-03-04 12:12:10 +0530

    fixed conflicts

--- lib/inconn2_service_web/controllers/dashboard_controller.ex
+++ lib/inconn2_service_web/controllers/dashboard_controller.ex
@@ -8,7 +8,6 @@ defmodule Inconn2ServiceWeb.DashboardController do
     render(conn, "energy_meter.json", energy_meter_data: energy_meter_data)
   end
 
-<<<<<<< HEAD
   def get_workflow_ticket_pie_chart(conn, _) do
     workflow_data = Dashboards.ticket_linear_chart(conn.assigns.sub_domain_prefix, conn.query_params)
     render(conn, "workflow_linear_data.json", workflow_data: workflow_data)
@@ -17,11 +16,11 @@ defmodule Inconn2ServiceWeb.DashboardController do
   def get_workflow_workorder_pie_chart(conn, _) do
     workflow_data = Dashboards.work_order_linear_chart(conn.assigns.sub_domain_prefix, conn.query_params)
     render(conn, "workflow_linear_data.json", workflow_data: workflow_data)
-=======
+  end
+
   def get_energy_meter_speedometer(conn, _) do
     energy_meter_data = Dashboards.get_energy_meter_speedometer_random(conn.query_params, conn.assigns.sub_domain_prefix)
     render(conn, "energy_meter.json", energy_meter_data: energy_meter_data)
->>>>>>> 291bd7cb21d10439db017934bc2221670f932edc
   end
   # def get_work_order_pie_chart(conn, _) do
   #   work_order_counts = Dashboard.work_order_pie_chart(conn.assigns.sub_domain_prefix, conn.query_params)
