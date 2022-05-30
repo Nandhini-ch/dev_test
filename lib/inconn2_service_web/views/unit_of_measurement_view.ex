356bcd7ecdec3f3fce51344ea027c50a63b05901
Minat Silvester K <minatsilvester>
2022-05-30 13:56:51 +0530

    Preloaded uom categeories and introduced query params

--- lib/inconn2_service_web/views/unit_of_measurement_view.ex
+++ lib/inconn2_service_web/views/unit_of_measurement_view.ex
@@ -1,6 +1,6 @@
 defmodule Inconn2ServiceWeb.UnitOfMeasurementView do
   use Inconn2ServiceWeb, :view
-  alias Inconn2ServiceWeb.UnitOfMeasurementView
+  alias Inconn2ServiceWeb.{UomCategoryView, UnitOfMeasurementView}
 
   def render("index.json", %{unit_of_measurements: unit_of_measurements}) do
     %{data: render_many(unit_of_measurements, UnitOfMeasurementView, "unit_of_measurement.json")}
@@ -14,6 +14,6 @@ defmodule Inconn2ServiceWeb.UnitOfMeasurementView do
     %{id: unit_of_measurement.id,
       name: unit_of_measurement.name,
       unit: unit_of_measurement.unit,
-      uom_category_id: unit_of_measurement.uom_category_id}
+      uom_category: render_one(unit_of_measurement.uom_category, UomCategoryView, "uom_category.json")}
   end
 end
