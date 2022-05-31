4710e42db328aa8e107ecc999ed1d2cf6ee261bc
EstharAlex <74195278+EstharAlex@users.noreply.github.com>
2022-05-31 17:49:25 +0530

    discarding apk_versions

---
+++ priv/repo/migrations/20220531061240_create_apk_versions.exs
@@ -0,0 +1,14 @@
+defmodule Inconn2Service.Repo.Migrations.CreateApkVersions do
+  use Ecto.Migration
+
+  def change do
+    create table(:apk_versions) do
+      add :version_no, :string
+      add :description, :text
+
+
+      timestamps()
+    end
+
+  end
+end
