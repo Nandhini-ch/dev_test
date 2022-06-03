29309b8864998f84495ca40ef904b823acbc5d29
Harisudhan V R <ravihari215@gmail.com>
2022-06-03 12:14:48 +0530

    Update inventory_management.ex

--- lib/inconn2_service/inventory_management.ex
+++ lib/inconn2_service/inventory_management.ex
@@ -113,7 +113,7 @@ defmodule Inconn2Service.InventoryManagement do
   end
 
   # Context functions for %Store{}
-  def list_stores(prefix, query_params) do
+  def list_stores(query_params, prefix) do
     from(s in Store, where: s.active and s.person_or_location_based == ^query_params["type"])
       |> Repo.all(prefix: prefix)
       |> Stream.map(fn store -> preload_user_for_store(store, prefix) end)
