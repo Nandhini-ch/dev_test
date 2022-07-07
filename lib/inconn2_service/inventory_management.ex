35ac03127893ea8365223e316543d809001a41f3
Minat Silvester K <minatsilvester>
2022-07-07 12:53:07 +0530

    Fixed approval error

--- lib/inconn2_service/inventory_management.ex
+++ lib/inconn2_service/inventory_management.ex
@@ -137,7 +137,7 @@ defmodule Inconn2Service.InventoryManagement do
 
       _ , query ->
         query
-    end)
+    end) |> add_active_condition()
     Repo.all(query, prefix: prefix)
       |> Stream.map(fn store -> preload_user_for_store(store, prefix) end)
       |> Enum.map(fn store -> preload_site_and_location_for_store(store, prefix) end)
@@ -336,7 +336,7 @@ defmodule Inconn2Service.InventoryManagement do
   end
 
   def list_transactions_to_be_approved(user, prefix) do
-    from(t in Transaction, where: t.approve_user_id == ^user.id and t.is_approve == "NA")
+    from(t in Transaction, where: t.approver_user_id == ^user.id and t.is_approved == "NA")
     |> Repo.all(prefix: prefix)
     |> Stream.map(fn t -> load_approver_user_for_transaction(t, prefix) end)
     |> Enum.map(fn t -> load_transaction_user_for_transaction(t, prefix) end)
@@ -502,14 +502,14 @@ defmodule Inconn2Service.InventoryManagement do
     end
   end
 
-  defp stock_query(query, %{}), do: add_active_condition(query)
+  defp stock_query(query, %{}), do: query
 
   defp stock_query(query, query_params) do
     Enum.reduce(query_params, query, fn
       {"item_id", item_id}, query -> from q in query, where: q.inventory_item_id == ^item_id
       {"store_id", store_id}, query -> from q in query, where: q.store_id == ^store_id
       _, query -> query
-    end) |> add_active_condition()
+    end)
   end
 
   defp inventory_supplier_item_query(query, %{}), do: query
@@ -534,7 +534,7 @@ defmodule Inconn2Service.InventoryManagement do
     end
   end
 
-  defp transactions_query(query, %{}), do: add_active_condition(query)
+  defp transactions_query(query, %{}), do: query
 
   defp transactions_query(query, query_params) do
     Enum.reduce(query_params, query, fn
@@ -542,7 +542,7 @@ defmodule Inconn2Service.InventoryManagement do
        {"unit_of_measurement_id", uom_id}, query -> from q in query, where: q.unit_of_measurement_id == ^uom_id
        {"supplier_id", supplier_id}, query -> from q in query, where: q.inventory_supplier_id == ^supplier_id
        _ , query -> query
-    end) |> add_active_condition()
+    end)
   end
 
   defp uom_conversion_query(from_uom_id, to_uom_id) do
