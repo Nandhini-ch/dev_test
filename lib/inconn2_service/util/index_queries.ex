759112ded395523162c0b79fc913b79a12d4b368
Minat Silvester K <minatsilvester>
2022-07-28 15:38:41 +0530

    Query params for check

--- lib/inconn2_service/util/index_queries.ex
+++ lib/inconn2_service/util/index_queries.ex
@@ -20,32 +20,44 @@ defmodule Inconn2Service.Util.IndexQueries do
 
   def employee_rosters_query(query, query_params) do
     Enum.reduce(query_params, query, fn
-    {"site_id", site_id}, query -> from q in query, where: q.site_id == ^site_id
-    _, query -> query end)
+      {"site_id", site_id}, query -> from q in query, where: q.site_id == ^site_id
+      _, query -> query
+    end)
   end
 
   def shift_query(query, query_params) do
     Enum.reduce(query_params, query, fn
-    {"site_id", site_id}, query -> from q in query, where: q.site_id == ^site_id
-    _, query -> query end)
+      {"site_id", site_id}, query -> from q in query, where: q.site_id == ^site_id
+      _, query -> query
+    end)
   end
 
   def store_query(query, query_params) do
     Enum.reduce(query_params, query, fn
-    {"site_id", site_id}, query -> from q in query, where: q.site_id == ^site_id
-    _, query -> query end)
+      {"site_id", site_id}, query -> from q in query, where: q.site_id == ^site_id
+      _, query -> query
+    end)
   end
 
   def equipment_query(query, query_params) do
     Enum.reduce(query_params, query, fn
-    {"site_id", site_id}, query -> from q in query, where: q.site_id == ^site_id
-    _, query -> query end)
+      {"site_id", site_id}, query -> from q in query, where: q.site_id == ^site_id
+      _, query -> query
+    end)
   end
 
   def location_query(query, query_params) do
     Enum.reduce(query_params, query, fn
-    {"site_id", site_id}, query -> from q in query, where: q.site_id == ^site_id
-    _, query -> query end)
+      {"site_id", site_id}, query -> from q in query, where: q.site_id == ^site_id
+      _, query -> query
+    end)
+  end
+
+  def check_query(query, query_params) do
+    Enum.reduce(query_params, query, fn
+      {"check_type_id", check_type_id}, query -> from q in query, where: q.check_type_id == ^check_type_id
+      _, query -> query
+    end)
   end
 
   defp get_subtree_zone_ids(zone_id, prefix) do
