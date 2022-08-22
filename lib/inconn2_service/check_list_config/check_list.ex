6a2a90ebd2623f1ab4a559361544138c2f535859
Harisudhan V R <ravihari215@gmail.com>
2022-08-23 04:53:15 +0530

    Update check_list.ex

--- lib/inconn2_service/check_list_config/check_list.ex
+++ lib/inconn2_service/check_list_config/check_list.ex
@@ -18,7 +18,7 @@ defmodule Inconn2Service.CheckListConfig.CheckList do
     |> cast(attrs, [:name, :type, :check_ids, :active, :site_id])
     |> validate_required([:name, :type, :check_ids])
     |> validate_inclusion(:type, ["PRE", "WP", "LOTO"])
-    |> validate_site_id()
+    # |> validate_site_id()
   end
 
   defp validate_site_id(cs) do
