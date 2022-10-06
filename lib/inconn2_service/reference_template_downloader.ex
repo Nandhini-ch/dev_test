8f21a064441517e1ef2bdb8e9378b98b95e0ddc7
Nandhini <brnstuff@gmail.com>
2022-10-06 11:43:46 +0530

    update template

--- lib/inconn2_service/reference_template_downloader.ex
+++ lib/inconn2_service/reference_template_downloader.ex
@@ -1,13 +1,14 @@
 defmodule Inconn2Service.ReferenceTemplateDownloader do
 
   alias Inconn2Service.AssetConfig
-  alias Inconn2Service.Workorder
+  # alias Inconn2Service.Workorder
   alias Inconn2Service.WorkOrderConfig
   alias Inconn2Service.CheckListConfig
-  alias Inconn2Service.Staff
-  alias Inconn2Service.Assignment
-  alias Inconn2Service.Settings
-  alias Inconn2Service.Inventory
+  # alias Inconn2Service.Staff
+  # alias Inconn2Service.Assignment
+  # alias Inconn2Service.Settings
+  # alias Inconn2Service.Inventory
+  alias Inconn2Service.CheckListConfig
 
 
   def download_asset_categories(prefix) do
@@ -23,5 +24,44 @@ defmodule Inconn2Service.ReferenceTemplateDownloader do
     final_report = header ++ body
     final_report
   end
+
+  def download_task_lists(prefix) do
+    task_lists = WorkOrderConfig.list_task_lists(prefix)
+
+    header = [["id", "reference", "Name", "Task Ids", "Asset Category Id"]]
+
+    body =
+      Enum.map(task_lists, fn r ->
+        [r.id, "", r.name, convert_array_of_integers_to_string(r.task_ids), r.asset_category_id]
+      end)
+
+      final_report = header ++ body
+      final_report
+  end
+
+  def download_check_lists(prefix) do
+    check_lists = CheckListConfig.list_check_lists(prefix)
+
+    header = [["id", "reference", "Name", "Type", "Check Ids"]]
+
+    body =
+      Enum.map(check_lists, fn r ->
+        [r.id, "", r.name, r.type, convert_array_of_integers_to_string(r.check_ids)]
+      end)
+
+      final_report = header ++ body
+      final_report
+  end
+
+  defp convert_array_of_integers_to_string(array_of_ids) do
+    if array_of_ids != nil do
+        array_of_ids
+        |> Enum.map(fn id -> to_string(id) end)
+        |> Enum.join(",")
+    else
+      ""
+    end
+  end
+
 
 end
