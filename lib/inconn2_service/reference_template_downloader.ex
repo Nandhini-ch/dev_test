48f3e766a354ae56f650e79276ce954e27c80ef0
Nandhini <brnstuff@gmail.com>
2022-10-06 15:03:37 +0530

    some functions in template downloader

--- lib/inconn2_service/reference_template_downloader.ex
+++ lib/inconn2_service/reference_template_downloader.ex
@@ -4,11 +4,11 @@ defmodule Inconn2Service.ReferenceTemplateDownloader do
   # alias Inconn2Service.Workorder
   alias Inconn2Service.WorkOrderConfig
   alias Inconn2Service.CheckListConfig
-  # alias Inconn2Service.Staff
+  alias Inconn2Service.Staff
   # alias Inconn2Service.Assignment
   # alias Inconn2Service.Settings
   # alias Inconn2Service.Inventory
-  alias Inconn2Service.CheckListConfig
+
 
 
   def download_asset_categories(prefix) do
@@ -40,7 +40,7 @@ defmodule Inconn2Service.ReferenceTemplateDownloader do
   end
 
   def download_check_lists(prefix) do
-    check_lists = CheckListConfig.list_check_lists(prefix)
+    check_lists = CheckListConfig.list_check_lists(prefix, %{})
 
     header = [["id", "reference", "Name", "Type", "Check Ids"]]
 
@@ -53,6 +53,49 @@ defmodule Inconn2Service.ReferenceTemplateDownloader do
       final_report
   end
 
+  def download_users(prefix) do
+    user = Staff.list_users(prefix)
+
+    header = [["id", "reference", "Username", "Firstname", "Last Name", "Email", "Mobile No", "Role Id", "Party Id", "Employee Id"]]
+
+    body =
+      Enum.map(user, fn r ->
+        [r.id, "", r.username, r.first_name, r.last_name, r.email, r.mobile_no, convert_array_of_integers_to_string(r.role_id), r.party_id, r.employee_id]
+      end)
+
+      final_report = header ++ body
+      final_report
+  end
+
+  def download_zones(prefix) do
+    zone = AssetConfig.list_zones(prefix)
+
+    header = [["id", "reference", "Description", "Name", "Parent Id"]]
+
+    body =
+    Enum.map(zone, fn r ->
+      [r.id, "", r.description, r.name, r.parent_id]
+    end)
+
+    final_report = header ++ body
+    final_report
+  end
+
+  def download_sites(prefix) do
+    locations = AssetConfig.list_sites(prefix)
+
+    header = [["id", "reference", "Name", "Description", "Branch", "Area", "Latitude", "Longitude", "Fencing Radius", "Site Code", "Time Zone", "Zone Id", "Party Id", "Address Line 1", "Address Line 2", "City", "State", "Country", "Postcode", "Contact First Name", "Contact Last Name", "Contact Designation", "Contact Email", "Contact Mobile", "Contact Land Line"]]
+
+    body =
+      Enum.map(locations, fn r ->
+        [r.id, "", r.name, r.description, r.branch, r.area, r.latitude, r.longitude, r.fencing_radius, r.site_code, r.time_zone, r.zone_id, r.party_id, r.address.address_line1, r.address.address_line2, r.address.city, r.address.state, r.address.country, r.address.postcode, r.contact.first_name, r.contact.last_name, r.contact.designation, r.contact.email, r.contact.mobile, r.contact.land_line]
+      end)
+
+    final_report = header ++ body
+    final_report
+  end
+
+
   defp convert_array_of_integers_to_string(array_of_ids) do
     if array_of_ids != nil do
         array_of_ids
@@ -62,6 +105,4 @@ defmodule Inconn2Service.ReferenceTemplateDownloader do
       ""
     end
   end
-
-
 end
---
+++ lib/inconn2_service_web/controllers/reference_template_downloader_controller.ex
@@ -0,0 +1,8 @@
+defmodule Inconn2ServiceWeb.ReferenceTemplateDownloaderController do
+  use Inconn2ServiceWeb, :controller
+
+  alias Inconn2Service.ReferenceTemplateDownloader
+  action_fallback Inconn2ServiceWeb.FallbackController
+
+
+end
