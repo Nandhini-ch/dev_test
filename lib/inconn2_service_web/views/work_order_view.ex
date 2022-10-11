53d20ecc3cf3448c3bc917daf064d801efc36fc1
Minat Silvester K <minatsilvester>
2022-10-11 15:59:57 +0530

    Changed view

--- lib/inconn2_service_web/views/work_order_view.ex
+++ lib/inconn2_service_web/views/work_order_view.ex
@@ -55,6 +55,8 @@ defmodule Inconn2ServiceWeb.WorkOrderView do
       is_workorder_acknowledgement_required: work_order.is_workorder_acknowledgement_required,
       is_workpermit_required: work_order.is_workpermit_required,
       pause_resume_times: work_order.pause_resume_times,
+      remarks: work_order.remarks,
+      approver: work_order.approver,
       is_paused: work_order.is_paused
     }
   end
