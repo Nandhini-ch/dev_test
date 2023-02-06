268e1e23e3f40b2e7f5afedc4265f9895c908317
Harisudhan V R <ravihari215@gmail.com>
2023-02-06 15:44:06 +0530

    Error message in forget password

--- lib/inconn2_service/staff.ex
+++ lib/inconn2_service/staff.ex
@@ -570,7 +570,7 @@ defmodule Inconn2Service.Staff do
     IO.inspect("1323424")
     IO.inspect(user)
     case user do
-      nil ->  {:error, "OTP has been sent to registered mobile no"}
+      nil ->  {:error, "Username/Password are incorrect"}
       _ -> {:ok, user}
     end
   end
