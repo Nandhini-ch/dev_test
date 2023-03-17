559797c3b839c712ad3a78cd26cbbfba093cb827
Nandhini <brnstuff@gmail.com>
2023-03-17 10:59:20 +0530

    code constraint removed

--- priv/repo/tenant_migrations/20230316072308_add_unique_index.exs
+++ priv/repo/tenant_migrations/20230316072308_add_unique_index.exs
@@ -5,7 +5,7 @@ defmodule Inconn2Service.Repo.Migrations.AddUniqueIndex do
     create unique_index(:designations, :name,  where: active = true)
     create unique_index(:shifts, :code,  where: active = true)
     create unique_index(:roles, :name,  where: active = true)
-    create unique_index(:sites, :site_code,  where: active = true)
+    # create unique_index(:sites, :site_code,  where: active = true)
     create unique_index(:locations, :location_code,  where: active = true)
     create unique_index(:equipments, :equipment_code,  where: active = true)
     create unique_index(:uoms, :name,  where: active = true)
