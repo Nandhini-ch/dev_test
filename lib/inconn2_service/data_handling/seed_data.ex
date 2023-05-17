627986aac436ea036aae418e04a75f529bba3e03
Nandhini <brnstuff@gmail.com>
2023-05-17 11:43:09 +0530

    uom seed changes

--- lib/inconn2_service/data_handling/seed_data.ex
+++ lib/inconn2_service/data_handling/seed_data.ex
@@ -26,55 +26,12 @@ defmodule Inconn2Service.DataHandling.SeedData do
     |> String.split(split_regex(content))
     |> Enum.filter(fn s -> s != "" end)
     |> Enum.map(fn s -> process_individual(entity, s) end)
-
-  end
-
-  defp split_regex(content) do
-    cond do
-      String.contains?(content, "\r\n") -> "\r\n"
-      true -> "\n"
-    end
-  end
-
-  #process individual data  in the features file and make a map %{"name" => name, "code" => code} like this
-  defp process_individual(:features, string) do
-    {name, code} =
-      string
-      |> String.split(",")
-      |> List.to_tuple()
-
-    %{"name" => name, "code" => code}
-
-  end
-
-  #process individual data  in the role_profile file and make a map  %{"feature_code" => code, "feature_name" => name, "access" => access=="Y"} like this
-  defp process_individual(:role_profile, string) do
-    {name, code, access} =
-      string
-      |> String.split(",")
-      |> List.to_tuple()
-
-    %{"feature_code" => code, "feature_name" => name, "access" => access=="Y"}
-
-  end
-
-  #process individual data  in the public_uom file and make a map %{"uom_category" => uom_category, "uom_unit" => uom_unit, "description" => description} like this
-  defp process_individual(:public_uom, string) do
-    {uom_category, uom_unit, description} =
-      string
-      |> String.split(",")
-      |> List.to_tuple()
-
-    %{"uom_category" => uom_category, "uom_unit" => uom_unit, "description" => description}
-
   end
 
   #Seed features...
   def seed_features() do
     read_and_insert(Application.app_dir(:inconn2_service, "/priv/features/Features.csv"), :features)
     |> Enum.map(&(Common.create_feature(&1)))
-    # |> Stream.map(&Task.async(fn -> Common.create_feature(&1) end))
-    # |> Enum.map(&Task.await/1)
   end
 
   #Seed role profiles...
@@ -91,6 +48,26 @@ defmodule Inconn2Service.DataHandling.SeedData do
   def seed_public_uom() do
     read_and_insert(Application.app_dir(:inconn2_service, "/priv/features/Uom.csv"), :public_uom)
     |> Enum.map(&(Common.create_public_uom(&1)))
+  end
+
+  #seed message templates ...
+  def seed_message_templates() do
+    Application.app_dir(:inconn2_service, "priv/features/templates.json")
+    |> File.read!()
+    |> Jason.decode!()
+    |> Enum.map(fn attrs ->
+      Communication.create_message_templates(attrs)
+    end)
+  end
+
+  #seed alert and notifications reserve ..
+  def seed_alert_and_notifications_reserve() do
+    Application.app_dir(:inconn2_service, "priv/features/alert.json")
+    |> File.read!()
+    |> Jason.decode!()
+    |> Enum.map(fn attrs ->
+      Common.create_alert_notification_reserve(attrs)
+    end)
   end
 
   def update_hierarchy_id_in_role_profile(prefix) do
@@ -139,14 +116,6 @@ defmodule Inconn2Service.DataHandling.SeedData do
 
       Enum.map(prefixes, &(update_existing_role_profiles(&1)))
       Enum.map(prefixes, &(update_existing_roles(&1)))
-
-    # prefixes
-    # |> Enum.map(&Task.async(fn -> update_existing_role_profiles(&1) end))
-    # |> Enum.map(&Task.await/1)
-
-    # prefixes
-    # |> Enum.map(&Task.async(fn -> update_existing_roles(&1) end))
-    # |> Enum.map(&Task.await/1)
   end
 
   def update_existing_role_profiles(prefix) do
@@ -164,6 +133,14 @@ defmodule Inconn2Service.DataHandling.SeedData do
     |> Enum.map(fn role ->
          update_role_permissions(role.role_profile_id, role, prefix)
        end)
+  end
+
+  #common function to seed all
+  def seed_data() do
+    SeedData.seed_features()
+    SeedData.seed_message_templates()
+    SeedData.seed_public_uom()
+    SeedData.seed_alert_and_notifications_reserve()
   end
 
   defp update_role_permissions(role_profile_id, role, prefix) do
@@ -193,33 +170,43 @@ defmodule Inconn2Service.DataHandling.SeedData do
     }
   end
 
-  #seed message templates ...
-  def seed_message_templates() do
-    Application.app_dir(:inconn2_service, "priv/features/templates.json")
-    |> File.read!()
-    |> Jason.decode!()
-    # |> IO.inspect()
-    |> Enum.map(fn attrs ->
-      Communication.create_message_templates(attrs)
-    end)
+  defp split_regex(content) do
+    cond do
+      String.contains?(content, "\r\n") -> "\r\n"
+      true -> "\n"
+    end
   end
 
-  #seed alert and notifications reserve ..
-  def seed_alert_and_notifications_reserve() do
-    Application.app_dir(:inconn2_service, "priv/features/alert.json")
-    |> File.read!()
-    |> Jason.decode!()
-    |> Enum.map(fn attrs ->
-      Common.create_alert_notification_reserve(attrs)
-    end)
+  #process individual data  in the features file and make a map %{"name" => name, "code" => code} like this
+  defp process_individual(:features, string) do
+    {name, code} =
+      string
+      |> String.split(",")
+      |> List.to_tuple()
+
+    %{"name" => name, "code" => code}
+
   end
 
-  #common function to seed all
-  def seed_data() do
-    SeedData.seed_features()
-    SeedData.seed_message_templates()
-    SeedData.seed_public_uom()
-    SeedData.seed_alert_and_notifications_reserve()
+  #process individual data  in the role_profile file and make a map  %{"feature_code" => code, "feature_name" => name, "access" => access=="Y"} like this
+  defp process_individual(:role_profile, string) do
+    {name, code, access} =
+      string
+      |> String.split(",")
+      |> List.to_tuple()
+
+    %{"feature_code" => code, "feature_name" => name, "access" => access=="Y"}
+
   end
 
+  #process individual data  in the public_uom file and make a map %{"uom_category" => uom_category, "uom_unit" => uom_unit, "description" => description} like this
+  defp process_individual(:public_uom, string) do
+    {uom_category, uom_unit, description} =
+      string
+      |> String.split(",")
+      |> List.to_tuple()
+
+    %{"uom_category" => uom_category, "uom_unit" => uom_unit, "description" => description}
+
+  end
 end
