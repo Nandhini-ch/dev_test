031fa1270fa289dcdcd43d8a321398ddd3214a2d
Harisudhan V R <ravihari215@gmail.com>
2023-05-19 10:15:56 +0530

    unique constraint for alert config

--- priv/repo/tenant_migrations/20230518082617_add_partial_unique_index.exs
+++ priv/repo/tenant_migrations/20230518082617_add_partial_unique_index.exs
@@ -2,6 +2,6 @@ defmodule Inconn2Service.Repo.Migrations.AddPartialUniqueIndex do
   use Ecto.Migration
 
   def change do
-    create unique_index(:alert_notification_configs, [:site_id, :alert_notification_reserve_id], where: "active = 'true'")
+    create unique_index(:alert_notification_configs, [:site_id, :alert_notification_reserve_id], where: "active = 'true'", name: :unique_alert_config)
   end
 end
