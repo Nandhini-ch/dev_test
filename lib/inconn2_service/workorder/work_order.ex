4d5d61dae3c41d7967da58fdaef9678f2609e8b5
Harisudhan V R <ravihari215@gmail.com>
2023-06-03 16:39:42 +0530

    update scheduled end in workorder

--- lib/inconn2_service/workorder/work_order.ex
+++ lib/inconn2_service/workorder/work_order.ex
@@ -59,7 +59,7 @@ defmodule Inconn2Service.Workorder.WorkOrder do
                     :workpermit_obtained_from_user_ids, :is_workorder_approval_required, :is_workpermit_required,
                     :is_workorder_acknowledgement_required, :workorder_acknowledgement_user_id, :is_loto_required, :loto_lock_check_list_id,
                     :loto_release_check_list_id, :loto_checker_user_id, :scheduled_end_date, :scheduled_end_time, :is_deactivated, :deactivated_date_time, :pause_resume_times, :is_paused, :pre_check_required, :cost])
-    |> validate_required([:asset_id, :type, :scheduled_date, :scheduled_time, :scheduled_end_date, :scheduled_end_date, :workorder_template_id])
+    |> validate_required([:asset_id, :type, :scheduled_date, :scheduled_time, :workorder_template_id])
     |> validate_inclusion(:type, ["PRV", "BRK", "TKT", "IOT"])
     |> validate_start_date_time()
     # |> validate_start_date()
