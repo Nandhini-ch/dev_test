e3230545b7152aa0b3a83631cab989af3f8234b6
Harisudhan V R <ravihari215@gmail.com>
2023-06-09 13:15:11 +0530

    inspect

--- lib/inconn2_service_web/controllers/iot_service/asset_controller.ex
+++ lib/inconn2_service_web/controllers/iot_service/asset_controller.ex
@@ -8,7 +8,8 @@ defmodule Inconn2ServiceWeb.IotService.AssetController do
   end
 
   def add_device_to_asset(conn, %{"asset_id" => asset_id, "asset_type" => asset_type, "device_key" => device_key, "device_id" => device_id, "licensee_prefix" => prefix}) do
-    Asset.add_device_to_asset(asset_type, asset_id, {device_key, device_id}, "inc_" <> prefix)
+    IO.inspect(conn.query_params, label: "Asset update from IOT params")
+    Asset.add_device_to_asset(asset_type, asset_id, {device_key, device_id}, "inc_" <> prefix) |> IO.inspect()
     render(conn, "success.json", data: "success")
   end
 end
